1eabe9f10ef716e393b5d28fb181bd6187404115
elisaveta9 <lizashishova9@gmail.com>
2023-12-07 15:19:14 +0300

    Refactor ConjunctiveNormalForm.java, add removePair. Refactor Pair.java

--- src/entities/ConjunctiveNormalForm.java
+++ src/entities/ConjunctiveNormalForm.java
@@ -1,16 +1,13 @@
 package entities;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class ConjunctiveNormalForm {
 
-    private final HashMap<String, HashSet<Integer>> literals = new HashMap<>();
-    private final HashMap<Integer, Pair> pairHashMap = new HashMap<>();
-    private Integer idDisjunction = 0;
+    private HashMap<String, HashSet<Integer>> literals = new HashMap<>();
+    private HashMap<Integer, Pair> pairHashMap = new HashMap<>();
+    private int idPair = 0;
 
     public ConjunctiveNormalForm(String str) {
         String regex = "^(([!-]?\\w+)|(\\([!-]?\\w+(\\|\\||\\\\/|\\+)[!-]?\\w+\\))(/\\\\|\\*|&&)?)+$";
@@ -20,6 +17,12 @@ public class ConjunctiveNormalForm {
         parseStr(str);
     }
 
+    public ConjunctiveNormalForm(ConjunctiveNormalForm cnf) {
+        literals = new HashMap<>(cnf.literals);
+        pairHashMap = new HashMap<>(cnf.pairHashMap);
+        idPair = cnf.idPair;
+    }
+
     private void parseStr(String str) {
         ArrayList<String> pairs = new ArrayList<>();
         for (String subst : Arrays.stream(str.split("\\(")).filter(e -> !e.trim().isEmpty()).toArray(String[]::new)) {
@@ -27,26 +30,39 @@ public class ConjunctiveNormalForm {
                 String[] value = Arrays.stream(pair.split("(\\|\\||\\\\/|\\+)|(/\\\\|\\*|&&)")).filter(e -> !e.trim().isEmpty()).toArray(String[]::new);
                 if (value.length > 0) {
                     pairHashMap.put(
-                            idDisjunction++,
+                            idPair++,
                             new Pair(
                                     Pair.DISJUNCTION,
                                     value
                             )
                     );
-                    addLiteral(pairHashMap.get(idDisjunction - 1).getLiteral1(), idDisjunction - 1);
-                    addLiteral(pairHashMap.get(idDisjunction - 1).getLiteral2(), idDisjunction - 1);
+                    addLiteral(pairHashMap.get(idPair - 1).getLiteral1(), idPair - 1);
+                    addLiteral(pairHashMap.get(idPair - 1).getLiteral2(), idPair - 1);
                 }
             }
         }
     }
 
-    private void addLiteral(String literal, Integer idPair) {
+    private void addLiteral(String literal, Integer id) {
         if (literal != null && !literal.isEmpty()) {
             HashSet<Integer> pairs = new HashSet<>();
             if (literals.containsKey(literal)) {
                 pairs = literals.get(literal);
             }
-            pairs.add(idPair);
+            pairs.add(id);
+            literals.put(
+                    literal, pairs
+            );
+        }
+    }
+
+    private void removePairLiteral(String literal, Integer id) {
+        if (literal != null && !literal.isEmpty()) {
+            HashSet<Integer> pairs = new HashSet<>();
+            if (literals.containsKey(literal)) {
+                pairs = literals.get(literal);
+            }
+            pairs.remove(id);
             literals.put(
                     literal, pairs
             );
@@ -54,10 +70,21 @@ public class ConjunctiveNormalForm {
     }
 
     public void addPair(Pair pair) {
-        pairHashMap.put(idDisjunction, pair);
-        addLiteral(pair.getLiteral1(), idDisjunction);
-        addLiteral(pair.getLiteral2(), idDisjunction);
-        idDisjunction++;
+        pairHashMap.put(idPair, pair);
+        addLiteral(pair.getLiteral1(), idPair);
+        addLiteral(pair.getLiteral2(), idPair);
+        idPair++;
+    }
+
+    public boolean removePair(Integer id) {
+        if (!pairHashMap.containsKey(id)) {
+            return false;
+        }
+        Pair pair = pairHashMap.get(id);
+        pairHashMap.remove(id);
+        removePairLiteral(pair.getLiteral1(), id);
+        removePairLiteral(pair.getLiteral2(), id);
+        return true;
     }
 
     public HashMap<String, HashSet<Integer>> getLiterals() {
@@ -66,5 +93,33 @@ public class ConjunctiveNormalForm {
 
     public HashMap<Integer, Pair> getPairHashMap() {
         return pairHashMap;
+    }
+
+    @Override
+    public String toString() {
+        boolean hasPrev = false;
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<Integer, Pair> set : pairHashMap.entrySet()) {
+            if (hasPrev) {
+                result.append("*");
+            } else {
+                hasPrev = true;
+            }
+            result.append(set.getValue());
+        }
+        return result.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ConjunctiveNormalForm that = (ConjunctiveNormalForm) o;
+        return Objects.equals(literals, that.literals) && Objects.equals(pairHashMap, that.pairHashMap) && Objects.equals(idPair, that.idPair);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(literals, pairHashMap, idPair);
     }
 }
